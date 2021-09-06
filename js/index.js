fcde5e353911b7e4913b1c210eed645b06e3c15f
zhangboheng <43784936+zhangboheng@users.noreply.github.com>
2021-09-06 15:46:53 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    var init;
     $('.stylebtn:eq(0)').on('click', function() {
-        if (init == 1) {
+        if ($('input[name=radioName]:checked', '#selectform').val() == 1) {
             window.location.href = "routes/countries.html";
-        } else if (init == 2) {
+        } else if ($('input[name=radioName]:checked', '#selectform').val() == 2) {
             window.location.href = "routes/language.html";
         } else {
             window.location.href = "routes/category.html";
